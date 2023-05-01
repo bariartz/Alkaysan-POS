f8c34a6454ceb7c316177bb8131f681ff2620357
Muhammad Zulki Akbari <52082691+bariartz@users.noreply.github.com>
2023-05-01 21:58:18 +0700

    Delete routes directory

--- routes/product.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['middleware' => ['auth']], function() {
-  Route::group(['prefix' => '{cabang}/product'], function() {
-    Route::get('/', 'TransaksiController@master_item');
-  });
-});
